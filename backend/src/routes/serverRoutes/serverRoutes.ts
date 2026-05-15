fc15fdbb26e6b903e47d46b7dcde9303177b0c09
Paradox <falloutblob@gmail.com>
2026-05-15 20:09:30 +0300

    remove getCookie route

--- backend/src/routes/serverRoutes/serverRoutes.ts
+++ backend/src/routes/serverRoutes/serverRoutes.ts
@@ -7,7 +7,6 @@ import {
   removeCreditsRoute,
 } from '../../controllers/Controllers/serverController';
 import { clearCookie } from '../../hooks/clearCookie';
-import { getCookie } from '../../hooks/getCookie';
 import multer from 'multer';
 
 const serverRouter = express.Router();
@@ -16,7 +15,6 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 serverRouter.post('/processFile', upload.single('file'), convertImage);
 serverRouter.post('/clearCookie', clearCookie);
-serverRouter.post('/getCookie', getCookie);
 serverRouter.post('/removeCredits', removeCreditsRoute);
 serverRouter.post('/create-checkout-session', checkoutSession);
 serverRouter.post('/getCheckoutSessionInfo', getCheckoutInfo);
